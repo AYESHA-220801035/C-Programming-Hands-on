c35d853f4f7dd62e2fd3cf1183f0b8de47c42da7
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 23:40:41 +0530

    Update 17_Substring_Index.c

--- 17_Substring_Index.c
+++ 17_Substring_Index.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-
-int main(){
-    char str[100], sub[100];
-    fgets(str,100,stdin);
-    fgets(sub,100,stdin);
-
-    for(int i=0;str[i];i++){
-        int j;
-        for(j=0;sub[j] && str[i+j]==sub[j];j++);
-        if(sub[j]=='\0'){
-            printf("Found at %d\n",i);
-            return 0;
+int findSubs(char str[], char sub[]) {
+    int i, j;
+    for (i = 0; str[i] != '\0'; i++) {
+        for (j = 0; sub[j] != '\0'; j++) {
+            if (str[i + j] != sub[j])
+                break;
         }
+        if (sub[j] == '\0')  
+            return i;
     }
-    printf("Not Found\n");
+    return -1;
+}
+int main() {
+    char str[100], sub[100];
+    fgets(str, sizeof(str), stdin);
+    fgets(sub, sizeof(sub), stdin);
+    sub[strcspn(sub, "\n")] = '\0';
+    int index = findSubs(str, sub);
+    if (index != -1)
+        printf("Substring found at index %d\n",index);
+    else
+        printf("Substring not found\n");
+    return 0;
 }
