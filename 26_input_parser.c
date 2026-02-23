1475f9f40667539baf9babf7dc846bdc3ab8d0fa
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 12:36:20 +0530

    Update 26_input_parser.c

--- 26_input_parser.c
+++ 26_input_parser.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 int parse(int val){
     if(val<1||val>100) return -1;
     return 0;
 }
-
 int main(){
     int x;
     if(scanf("%d",&x)!=1){printf("Format Error\n");return 1;}
