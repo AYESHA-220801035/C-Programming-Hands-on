b361e0d32197d180ce241cd90833e56a6608d60d
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 21:07:03 +0530

    Update 09_Reverse_Integer.c

--- 09_Reverse_Integer.c
+++ 09_Reverse_Integer.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
-
-int main() {
-    int n, rev=0;
-    scanf("%d", &n);
-
-    while(n!=0) {
-        rev = rev*10 + n%10;
-        n/=10;
+#include<stdio.h>
+#include<limits.h>
+int main(){
+    int n,rev=0,dig=0;
+    scanf("%d",&n);
+    while(n!=0){
+        dig=n%10;
+        if(rev>INT_MAX/10||rev<INT_MIN/10){
+            printf("overflow occurs");
+            return 0;
+        }
+        rev=rev*10+dig;
+        n=n/10;
     }
-
-    printf("Reversed: %d\n", rev);
+    printf("%d",rev);
     return 0;
 }
