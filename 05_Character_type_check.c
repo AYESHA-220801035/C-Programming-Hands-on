b9185ac261626d3065ddba4e38065af63e668fed
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 20:07:45 +0530

    Update 05_Character_type_check.c

--- 05_Character_type_check.c
+++ 05_Character_type_check.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
-
 int main() {
     char ch;
     scanf(" %c", &ch);
-
     if (isdigit(ch))
         printf("Digit\n");
     else if (isalpha(ch)) {
@@ -19,3 +17,4 @@ int main() {
 
     return 0;
 }
+
