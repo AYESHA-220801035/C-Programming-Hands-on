207f90daefbda34e86d02e6d514cd3485bd1419c
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 12:24:12 +0530

    Update and rename 09_bst_insert_search.c to 33_bst_insert_search.c

--- 33_bst_insert_search.c
+++ 33_bst_insert_search.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Node{
     int data;
     struct Node *left,*right;
 };
-
 struct Node* newNode(int val){
     struct Node* n=malloc(sizeof(struct Node));
     n->data=val;n->left=n->right=NULL;
     return n;
 }
-
 struct Node* insert(struct Node* root,int val){
     if(!root) return newNode(val);
     if(val<root->data) root->left=insert(root->left,val);
     else root->right=insert(root->right,val);
     return root;
 }
-
 void inorder(struct Node* root){
     if(root){
         inorder(root->left);
@@ -26,7 +22,6 @@ void inorder(struct Node* root){
         inorder(root->right);
     }
 }
-
 int main(){
     struct Node* root=NULL;
     int n,x;
