dec1893cfcdd6efbc704785453360701f929e8f8
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 12:27:28 +0530

    Update and rename 14_bst_delete_height.c to 38_bst_delete_height.c

--- 38_bst_delete_height.c
+++ 38_bst_delete_height.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Node{
     int data;
     struct Node *left,*right;
 };
-
 struct Node* newNode(int v){
     struct Node* n=malloc(sizeof(struct Node));
     n->data=v;n->left=n->right=NULL;
     return n;
 }
-
 struct Node* insert(struct Node* r,int v){
     if(!r) return newNode(v);
     if(v<r->data) r->left=insert(r->left,v);
     else r->right=insert(r->right,v);
     return r;
 }
-
 int height(struct Node* r){
     if(!r) return 0;
     int l=height(r->left);
     int h=height(r->right);
     return (l>h?l:h)+1;
 }
-
 int main(){
     struct Node* root=NULL;
     int n,x;
