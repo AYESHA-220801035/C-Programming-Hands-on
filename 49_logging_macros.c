d586e0c1f708c9b05a304c5f68f63837be2eafdd
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 12:34:28 +0530

    Update and rename 25_logging_macros.c to 49_logging_macros.c

--- 49_logging_macros.c
+++ 49_logging_macros.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 #define LOG_DEBUG(msg) printf("DEBUG %s:%d %s\n",__FILE__,__LINE__,msg)
 #define LOG_INFO(msg) printf("INFO %s:%d %s\n",__FILE__,__LINE__,msg)
 #define LOG_ERR(msg) printf("ERROR %s:%d %s\n",__FILE__,__LINE__,msg)
-
 int main(){
     LOG_DEBUG("Start");
     LOG_INFO("Running");
