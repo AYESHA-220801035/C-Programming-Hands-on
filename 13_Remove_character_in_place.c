604e516d19a3f12bfbd9cdeeff69655ea4b08d50
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 22:49:00 +0530

    Update 13_Remove_character_in_place.c

--- 13_Remove_character_in_place.c
+++ 13_Remove_character_in_place.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main(){
-    char str[100], ch;
-    fgets(str,100,stdin);
-    scanf(" %c",&ch);
-
-    int i,j=0;
-    for(i=0;str[i]!='\0';i++)
-        if(str[i]!=ch)
-            str[j++]=str[i];
-
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int ch;
+    scanf("%c",&ch);
+    int i=0;int j=0;
+    while(str[i]!='\0'){
+        if(str[i]!=ch){
+            str[j]=str[i];j++;
+        }
+        i++;
+    }
     str[j]='\0';
-    printf("%s",str);
+    printf("Modified string is %s",str);
+    return 0;
 }
