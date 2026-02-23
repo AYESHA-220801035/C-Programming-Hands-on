d778c6086a6d0a81fd6aad450fb150ded827d1cc
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 12:25:39 +0530

    Update and rename 12_doubly_linked_list.c to 36_doubly_linked_list.c

--- 36_doubly_linked_list.c
+++ 36_doubly_linked_list.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Node{
     int seat;
     struct Node *prev,*next;
 };
-
 void display(struct Node* head){
     struct Node* temp=head;
     while(temp){printf("%d ",temp->seat);if(!temp->next)break;temp=temp->next;}
     while(temp){printf("%d ",temp->seat);temp=temp->prev;}
 }
-
 int main(){
     struct Node *head=NULL,*tail=NULL,*newnode;
     for(int i=0;i<4;i++){
