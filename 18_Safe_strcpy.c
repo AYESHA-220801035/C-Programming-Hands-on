188cd4904b1f7c17990e434a658e961f81c11863
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 23:48:21 +0530

    Update 18_Safe_strcpy.c

--- 18_Safe_strcpy.c
+++ 18_Safe_strcpy.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
-void safeCopy(char *dest, const char *src, int size){
+void safcpy(char *dest, const char *src, int size){
     int i;
     for(i=0;i<size-1 && src[i];i++)
         dest[i]=src[i];
     dest[i]='\0';
 }
-
 int main(){
     char src[100], dest[20];
     fgets(src,100,stdin);
-
-    safeCopy(dest,src,20);
+    safcpy(dest,src,20);
     printf("%s",dest);
 }
+
