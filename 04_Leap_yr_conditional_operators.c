bb7ed19d11cfc56e09ac9c0db4036aded31eaab3
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-23 19:54:54 +0530

    Update 04_Leap_yr_conditional_operators.c

--- 04_Leap_yr_conditional_operators.c
+++ 04_Leap_yr_conditional_operators.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-
 int main() {
-    int year;
-    scanf("%d", &year);
-
-    (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-        ? printf("Leap Year\n")
-        : printf("Not Leap Year\n");
-
+    int yr;
+    scanf("%d",&yr);
+    ((yr%400==0) ||(yr%4==0 && yr%100!=0))?printf("leapyr"):printf("not leap yr");
     return 0;
 }
