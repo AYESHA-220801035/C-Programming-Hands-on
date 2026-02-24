4b6663df46b72530bcbadb5f330605a7c6108ff7
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-24 21:07:21 +0530

    Update 24_sort_city_names.c

--- 24_sort_city_names.c
+++ 24_sort_city_names.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int main(){
-    int n;
-    printf("Enter number of cities: ");
-    scanf("%d",&n);
-    getchar();
-    char **cities=malloc(n*sizeof(char*));
-    for(int i=0;i<n;i++){
-        cities[i]=malloc(50);
-        fgets(cities[i],50,stdin);
-        cities[i][strcspn(cities[i],"\n")]=0;
-    }
-    for(int i=0;i<n-1;i++)
-        for(int j=i+1;j<n;j++)
-            if(strcmp(cities[i],cities[j])>0){
-                char *temp=cities[i];
-                cities[i]=cities[j];
-                cities[j]=temp;
+#define MAX 5     
+#define SIZE 100   
+void sortCities(char *cities[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (strcmp(cities[i], cities[j]) > 0) {
+                char *temp = cities[i];
+                cities[i] = cities[j];
+                cities[j] = temp;
             }
-    for(int i=0;i<n;i++){
-        printf("%s\n",cities[i]);
+        }
+    }
+}
+void freecit(char *cities[], int n) {
+    for (int i = 0; i < n; i++) {
         free(cities[i]);
     }
-    free(cities);
+}
+int main() {
+    char *cities[MAX];
+    printf("Enter %d city names:\n", MAX);
+    for (int i = 0; i < MAX; i++) {
+        cities[i] = (char *)malloc(SIZE * sizeof(char));
+        if (cities[i] == NULL) {
+            printf("Memory allocation failed!\n");
+            return 1;
+        }
+        fgets(cities[i], SIZE, stdin);
+        cities[i][strcspn(cities[i], "\n")] = '\0';
+    }
+    sortCities(cities, MAX);
+    printf("Cities in order:\n");
+    for (int i = 0; i < MAX; i++) {
+        printf("%s\n", cities[i]);
+    }
+    freecit(cities, MAX);
     return 0;
 }
