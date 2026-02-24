7a7dc08dbf48639672d091a8543c0dc5563154f8
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-24 09:17:36 +0530

    Update 20_Function_Pointer_Arithmetic.c

--- 20_Function_Pointer_Arithmetic.c
+++ 20_Function_Pointer_Arithmetic.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
 int add(int a,int b){ return a+b; }
 int sub(int a,int b){ return a-b; }
 int mul(int a,int b){ return a*b; }
-
+int div(int a,int b){ return a/b; }
 int main(){
     int a,b,choice;
     scanf("%d %d %d",&a,&b,&choice);
-
     int (*op)(int,int);
-
     if(choice==1) op=add;
     else if(choice==2) op=sub;
-    else op=mul;
-
+    else if (choice==3) op=mul;
+    else op=div;
     printf("Result: %d\n",op(a,b));
 }
+
