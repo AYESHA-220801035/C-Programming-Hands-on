ea54e8da02cf3d55bce2e007d2bcda27f65f70b1
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-24 11:00:22 +0530

    Update 22_custom_dynamic_array.c

--- 22_custom_dynamic_array.c
+++ 22_custom_dynamic_array.c
@@ -1,37 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 typedef struct{
     int *data;
-    int size;
-    int capacity;
-}Vector;
-
-void init(Vector *v){
-    v->capacity=2;
+    int size;int capacity;
+}vector;
+void initi(vector *v,int cap){
+    v->capacity=cap;
     v->size=0;
-    v->data=malloc(v->capacity*sizeof(int));
+    v->data=malloc(v->capacity*(sizeof(int)));
 }
-
-void resize(Vector *v){
-    v->capacity*=2;
-    v->data=realloc(v->data,v->capacity*sizeof(int));
+void resize(vector *v){
+    v->capacity=v->capacity*2;
+    v->data=realloc(v->data,v->capacity*(sizeof(int)));
 }
-
-void push(Vector *v,int val){
+void push(vector *v,int val){
     if(v->size==v->capacity) resize(v);
     v->data[v->size++]=val;
 }
-
-void pop(Vector *v){
-    if(v->size>0) v->size--;
+void pop(vector *v){
+    if(v->size>0)v->size--;
 }
-
 int main(){
-    Vector v; init(&v);
-    push(&v,10); push(&v,20); push(&v,30);
+    vector v;
+    int cap;
+    scanf("%d",&cap);
+        initi(&v,cap);
+    push(&v,10);
+    push(&v,20);
+    push(&v,30);
     pop(&v);
-    for(int i=0;i<v.size;i++) printf("%d ",v.data[i]);
+    for(int i=0;i<v.size;i++){
+        printf("%d\n",v.data[i]);
+    }
     free(v.data);
     return 0;
-}
+    }
+    
