9e55e99bf57f94f004b58082679c43f017203b3f
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-25 19:22:40 +0530

    Update 29_update_student_by_pointer.c

--- 29_update_student_by_pointer.c
+++ 29_update_student_by_pointer.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
 struct Student {
     int roll;
     char name[50];
     float marks;
 };
-
 void updateMarks(struct Student *s, float newMarks) {
     s->marks = newMarks;
 }
-
 int main() {
     struct Student s = {101, "Ravi", 75.0};
     printf("Before Update: %.2f\n", s.marks);
