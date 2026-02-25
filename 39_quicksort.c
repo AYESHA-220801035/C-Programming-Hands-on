7583e93cfe7cbefc1e7bd068c61b6d87f1387543
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-25 21:28:49 +0530

    Update 39_quicksort.c

--- 39_quicksort.c
+++ 39_quicksort.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = low - 1;
@@ -18,7 +16,6 @@ int partition(int arr[], int low, int high) {
     swap(&arr[i+1], &arr[high]);
     return i+1;
 }
-
 void quickSort(int arr[], int low, int high) {
     if(low < high) {
         int pi = partition(arr, low, high);
@@ -26,16 +23,13 @@ void quickSort(int arr[], int low, int high) {
         quickSort(arr, pi+1, high);
     }
 }
-
 int main() {
     int n;
     scanf("%d", &n);
     int arr[n];
     for(int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
     quickSort(arr, 0, n-1);
-
     for(int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     return 0;
