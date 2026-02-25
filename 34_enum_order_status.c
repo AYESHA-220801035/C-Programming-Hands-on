b707c7308d46f04b2337f2ece0c9caa10a2222df
AYESHA-220801035 <220801035@rajalakshmi.edu.in>
2026-02-25 20:02:10 +0530

    Update 34_enum_order_status.c

--- 34_enum_order_status.c
+++ 34_enum_order_status.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
-enum Status {PENDING, SHIPPED, DELIVERED};
+enum Status {PENDING,SHIPPED,DELIVERED};
+struct Order {
+    int orderID;
+    enum Status status;
+};
+void displayStatus(enum Status s) {
+    if (s == PENDING)
+        printf("PENDING\n");
+    else if (s == SHIPPED)
+        printf("SHIPPED\n");
+    else if (s == DELIVERED)
+        printf("DELIVERED\n");
+}
+void updateStatus(struct Order *o, enum Status newStatus) {
+    o->status = newStatus;
+}
 int main() {
-    enum Status s = PENDING;
-    printf("Initial Status: %d\n", s);
-    s = SHIPPED;
-    printf("Updated Status: %d\n", s);
+    struct Order o1 = {1001, PENDING};
+    printf("Initial Status: ");
+    displayStatus(o1.status);
+    updateStatus(&o1, SHIPPED);
+    printf("Updated Status: ");
+    displayStatus(o1.status);
+    updateStatus(&o1, DELIVERED);
+    printf("Final Status: ");
+    displayStatus(o1.status);
     return 0;
 }
